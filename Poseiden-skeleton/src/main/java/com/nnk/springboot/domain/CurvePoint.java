08464bc166c12aeae764c575db5cb9be86d8ded4
Pat <patrycja.kruczynska@gmail.com>
2026-03-27 16:55:06 +0100

    domain curvepoint

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,14 +1,88 @@
 package com.nnk.springboot.domain;
-
-import org.hibernate.validator.constraints.Length;
-
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "curvepoint")
+    private Integer id;
+
+    @NotNull(message ="Curve ID is required")
+    @Column(name = "curveid")
+    private Integer curveId;
+
+    @Column(name = "asofdate")
+    private Timestamp asOfDate;
+
+    @NotNull(message = "Term is required")
+    @DecimalMin("0.0")
+    @Column(name = "term")
+    private Double term;
+
+    @NotNull(message = "Value is required")
+    @DecimalMin("0.0")
+    @Column(name = "value")
+    private Double value;
+
+    @Column(name = "creationdate")
+    private Timestamp creationDate;
+
+    // constructor
+    public CurvePoint() {
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public Double getTerm() {
+        return term;
+    }
+
+    public Timestamp getAsOfDate() {
+        return asOfDate;
+    }
+
+    public Integer getCurveId() {
+        return curveId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setCurveId(Integer curveId) {
+        this.curveId = curveId;
+    }
+
+    public void setAsOfDate(Timestamp asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public void setTerm(Double term) {
+        this.term = term;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
 }
